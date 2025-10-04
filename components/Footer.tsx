e59d55f23c372b6dc5e865c8cf86dac2060b1dda
RajendraPancholi <rpancholi522@gmail.com>
2025-10-04 16:10:52 +0530

    dynamic date

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -29,7 +29,7 @@ const Footer = () => {
 
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center gap-2 mx-2">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Rajendra Pancholi
+          Copyright &copy; {new Date().getFullYear()} {" "} Rajendra Pancholi
         </p>
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
