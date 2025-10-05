e9885daf9e187063c99e04f1ce65aef50201a5ee
RajendraPancholi <rpancholi522@gmail.com>
2025-10-05 20:42:35 +0530

    adding some svgs and some changes to resolve issues.

--- data/index.ts
+++ data/index.ts
@@ -73,68 +73,23 @@ export const gridItems = [
 
 export const projects = [
   {
-    id: 1,
-    title: '3D Solar System Planets to Explore',
-    des: 'Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.',
-    img: '/p1.svg',
-    iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/fm.svg'],
-    link: '/ui.earth.com',
-  },
-  {
-    id: 2,
-    title: 'Yoom - Video Conferencing App',
-    des: 'Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.',
-    img: '/p2.svg',
-    iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/stream.svg', '/c.svg'],
-    link: '/ui.yoom.com',
-  },
-  {
-    id: 3,
-    title: 'AI Image SaaS - Canva Application',
-    des: 'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
-    img: '/p3.svg',
-    iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/c.svg'],
-    link: '/ui.aiimg.com',
-  },
-  {
-    id: 4,
-    title: 'Animated Apple Iphone 3D Website',
-    des: 'Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..',
-    img: '/p4.svg',
-    iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/three.svg', '/gsap.svg'],
-    link: '/ui.apple.com',
+    // id: 1,
+    // title: '3D Solar System Planets to Explore',
+    // des: 'Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.',
+    // img: '/p1.svg',
+    // iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/fm.svg'],
+    // link: '/ui.earth.com',
   },
 ];
 
 export const workExperience = [
   {
     id: 1,
-    title: 'Frontend Engineer Intern',
-    desc: 'Assisted in the development of a web-based platform using React.js, enhancing interactivity.',
+    title: 'MIS excecutive',
+    desc: 'Experienced MIS Executive skilled in Tally Prime data entry, cell tower electrical product management, and permission coordination. Proficient in data analysis, inventory control, and optimizing information systems for operational efficiency.',
     className: 'md:col-span-2',
     thumbnail: '/exp1.svg',
-  },
-  {
-    id: 2,
-    title: 'E-Commerce next-app',
-    desc: 'Designed and developed mobile app for both iOS & Android platforms using React Native.',
-    className: 'md:col-span-2', // change to md:col-span-2
-    thumbnail: '/exp2.svg',
-  },
-  {
-    id: 3,
-    title: ' Project',
-    desc: 'Led the dev of a mobile app for a client, from initial concept to deployment on app stores.',
-    className: 'md:col-span-2', // change to md:col-span-2
-    thumbnail: '/exp3.svg',
-  },
-  {
-    id: 4,
-    title: 'Modern UI App',
-    desc: 'Developed and maintained user-facing features using modern frontend technologies.',
-    className: 'md:col-span-2',
-    thumbnail: '/exp4.svg',
-  },
+  }
 ];
 
 export const socialMedia = [
@@ -159,4 +114,10 @@ export const iconList = [
   { img: '/three.svg' },
   { img: '/re.svg' },
   { img: '/fm.svg' },
+  {img: '/mndb.svg'},
+  {img: '/mysql.svg'},
+  {img: '/expjs.svg'},
+  {img: '/ndjs.svg'},
+  {img: '/py.svg'},
+
 ];
