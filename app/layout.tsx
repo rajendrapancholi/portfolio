4e7ad585794f2e060c9751c3634d39b1f97ffe66
Rajendra Pancholi <rpancholi522@gmail.com>
2026-02-08 02:58:38 +0530

    ready to deploy 2.1.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from 'next';
-import '@/styles/globals.css';
-import Providers from '@/components/Providers';
-import { generateDynamicJsonLd } from '@/lib/seo/jsonld';
-import { ENV } from '@/config/env';
-import { baseMetadata } from '@/lib/seo/metadata';
-import Script from 'next/script';
-import safeJSONStringify from '@/lib/utils/safeSanitize';
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+import Providers from "@/components/Providers";
+import { generateDynamicJsonLd } from "@/lib/seo/jsonld";
+import { ENV } from "@/config/env";
+import { baseMetadata } from "@/lib/seo/metadata";
+import Script from "next/script";
+import safeJSONStringify from "@/lib/utils/safeSanitize";
 
 export const metadata: Metadata = baseMetadata;
 
@@ -14,10 +14,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const websiteUrl = ENV.BASE_URL ?? 'https://rajendrapancholi.vercel.app';
+  const websiteUrl = ENV.BASE_URL ?? "https://rajendrapancholi.vercel.app";
 
   const jsonLd = generateDynamicJsonLd({
-    type: 'home',
+    type: "home",
     url: websiteUrl,
     logo: `${websiteUrl}/logo.png`,
   });
@@ -30,18 +30,14 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: safeJSONStringify(jsonLd) }}
         />
-        <Script id="x-meta-tags" strategy="beforeInteractive">
-          {`
-          <meta name="x:card" content="summary_large_image" />
-          <meta name="x:site" content="@rajendrapancholi" />
-          <meta name="x:creator" content="@rajendrapancholi" />
-        `}
-        </Script>
       </head>
       <body>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
+        <script
+          id="jsonld-global"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: safeJSONStringify(jsonLd) }}
+        />
       </body>
     </html>
   );
