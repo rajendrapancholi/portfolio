21a7ce39c208a8175cf77469fda2d1558936feb8
Rajendra Pancholi <rpancholi522@gmail.com>
2026-08-08 16:09:27 +0530

    fix(services): remove projectService.getBySlug, queries nonexistent field

--- src/lib/services/projectService.ts
+++ src/lib/services/projectService.ts
@@ -9,19 +9,9 @@ const getRecent = cache(async () => {
   const projects = await ProjectModel.find({}).sort({ _id: -1 }).lean();
   const projectsJson: Project[] = JSON.parse(JSON.stringify(projects));
   return projectsJson;
-  // return projects as Project[];
-});
-
-const getBySlug = cache(async (slug: string) => {
-  await connectToDB();
-  const product = await ProjectModel.findOne({ slug }).lean();
-  const productJson: Project[] = JSON.parse(JSON.stringify(product));
-  return productJson;
-  // return product as Project[];
 });
 
 const projectService = {
   getRecent,
-  getBySlug,
 };
 export default projectService;
